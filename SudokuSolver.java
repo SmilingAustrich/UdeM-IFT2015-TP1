29027787c929c4ecb16be749bcdad787092ccd13
Ilyesse Bouzamita <ilyesse.bouzamita@outlook.com>
2024-11-17 22:59:55 -0500

    Implémentation de la classe SudokuSolver, les commentaires sont détaillés et si tu ne comprends pas pose moi des questions,
    Il nous reste qu'à faire les commentaires de la classe LinkedGeneralTree et on a fini !

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -3,16 +3,15 @@ public class SudokuSolver implements GameSolver {
     private IntegerBoard solution;
     private Tree<IntegerBoard> solutionTree;
 
-    // Constructeur unique qui prend un GameBoard et initialise les autres attributs
+    // Constructeur qui prend un GameBoard et initialise les autres attributs
     public SudokuSolver(GameBoard<Integer> board) {
         this.board = (IntegerBoard) board;
-        this.solution = null; // Initialisation par défaut à null
-        this.solutionTree = new LinkedGeneralTree<>(); // Initialisation de l'arbre
-        this.solutionTree.setRoot(this.board); // Définir la racine de l'arbre comme la grille initiale
+        this.solution = null;
+        this.solutionTree = new LinkedGeneralTree<>();
+        this.solutionTree.setRoot(this.board); // Initialise la racine de l'arbre comme étant la grille initiale
     }
 
-    @Override
-    public boolean solve() {
+    public boolean solve(){
         return solveBoard();
     }
 
@@ -22,63 +21,70 @@ public class SudokuSolver implements GameSolver {
             System.out.println("Sudoku résolu :");
             solution.display();
         } else {
-            System.out.println("Aucune solution n'a été trouvée.");
+            System.out.println("Aucune solution n'a été trouvé");
         }
     }
 
-    // Méthode pour valider un placement dans la grille
-    public boolean isValidPlacement(int row, int col, Integer value) {
-        // Vérifie la ligne
-        for (int i = 0; i < board.getWidth(); i++) {
-            if (board.getCell(row, i).equals(value)) {
+    private boolean isValidPlacement (int row, int col, Integer value) {
+        // Vérification des lignes
+        for (int i = 0; i < board.getWidth(); i++ ) {
+            if (board.getCell(row,i).equals(value)){
                 return false;
             }
         }
 
-        // Vérifie la colonne
+        // Vérification des colonnes
         for (int i = 0; i < board.getHeight(); i++) {
-            if (board.getCell(i, col).equals(value)) {
+            if (board.getCell(i,col).equals(value)){
                 return false;
             }
         }
 
-        // Vérifie la sous-grille
-        int subGridSize = (int) Math.sqrt(board.getWidth());
-        int boxRowStart = (row / subGridSize) * subGridSize;
-        int boxColStart = (col / subGridSize) * subGridSize;
+        // Vérification de la sous grille
+        int sousGrilleTaille = (int) Math.sqrt(board.getWidth()); // ex : pour une grille 9*9, la largeur de la sous grille = 3
+        // Imaginons qu'on a une grille 4*4 et qu'on veut la sous-grille en bas à gauche, pour débuter l'analyse de
+        // cette sous-grille, on doit savoir on est à quel ligne et quelle colonne, sachant qu'on est dans une grille
+        // 4*4, on sera dans la 3ème ligne (index = 2) et 1ère colonne (index = 0), donc le calcul sera la suivante :
+        // (2/2) * 2 = 2, (0/2) * 2 = 0.
+        int sousGrilleLigne = (row/sousGrilleTaille) * sousGrilleTaille;
+        int sousGrilleColonne = (col/sousGrilleTaille) * sousGrilleTaille;
 
-        for (int i = boxRowStart; i < boxRowStart + subGridSize; i++) {
-            for (int j = boxColStart; j < boxColStart + subGridSize; j++) {
-                if (board.getCell(i, j).equals(value)) {
+        for (int i = sousGrilleLigne; i < sousGrilleLigne + sousGrilleTaille; i++) {
+            for (int j = sousGrilleColonne; j < sousGrilleColonne + sousGrilleTaille; j++){
+                if (board.getCell(i,j).equals(value)){
                     return false;
                 }
             }
         }
+        return true;
 
-        return true; // Le placement est valide
     }
 
-    // Méthode de résolution avec backtracking
-    private boolean solveBoard() {
-        for (int row = 0; row < board.getHeight(); row++) {
-            for (int col = 0; col < board.getWidth(); col++) {
-                if (board.getCell(row, col) == 0) { // Trouve une cellule vide
-                    for (int num = 1; num <= board.getWidth(); num++) {
-                        if (isValidPlacement(row, col, num)) {
-                            board.setCell(row, col, num);
-                            solutionTree.addChild(this.board, board); // Ajoute l'état à l'arbre
+    private boolean solveBoard(){
+        // Vérifier les cellules vides dans la grille
+        for (int row = 0; row < board.getWidth(); row++){
+            for (int col = 0; col < board.getHeight(); col++){
+                if (board.getCell(row,col).equals(0)){  // Présence de cellule vide
+                    for (int chiffre = 1; chiffre <= board.getWidth(); chiffre++){
+                        if (isValidPlacement(row,col,chiffre)){
+                            board.setCell(row,col,chiffre); // Si le placement est valide, le chiffre sera intégré
+                                                            // la cellule.
+                            solutionTree.addChild(this.board,board); // Sauvegarde l'état dans l'arbre
                             if (solveBoard()) {
-                                solution = board; // Enregistre la solution trouvée
+                                solution = board;
                                 return true;
                             }
-                            board.setCell(row, col, 0); // Backtracking
+                            // Concept de backtracking, si une cellule ne présente aucune solution, on retourne dans la
+                            // dernière cellule vide (qui a une solution) afin de l'initialiser à 0 pour explorer
+                            // d'autres possibilités.
+                            board.setCell(row,col,0);
                         }
                     }
-                    return false; // Retourne faux si aucune solution n'est trouvée pour cette cellule
+                    return false; // Si aucune solution n'est trouvée, retourne faux
                 }
             }
         }
-        return true; // Retourne vrai si la grille est entièrement remplie
+        return true; // Si aucune cellule vide n'est trouvé, la grille est n'est donc pas vide
     }
 }
 
